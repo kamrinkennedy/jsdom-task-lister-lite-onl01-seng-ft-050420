cf8b41c06855f081f624efb3fd77a43a1e7e2978
Kamrin <kamrinkennedy@gmail.com>
2020-08-07 14:00:02 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+
+const taskForm = document.getElementById('create-task-form');
+const taskList = document.getElementById('tasks');
+const text = document.getElementById('new-task-description');
+
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  taskForm.children[2].addEventListener('click', (e) => {
+    e.preventDefault()
+    console.log('This works')
+    if (text.value) {
+      taskList.innerHTML += `<li>${text.value}</li>`
+      text.value = ''
+    }
+  });
 });
+
